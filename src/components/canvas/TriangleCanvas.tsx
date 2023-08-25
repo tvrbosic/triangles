51f36779195d09955ee32a0d19563d9eed99bbff
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2023-08-25 11:41:59 +0200

    Triangle scailing and positioning improvements

--- src/components/canvas/TriangleCanvas.tsx
+++ src/components/canvas/TriangleCanvas.tsx
@@ -5,25 +5,48 @@ import { ITriangleCanvas } from 'components/canvas/types';
 export default function TriangleCanvas({ triangle, width, height }: ITriangleCanvas) {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
+  // Calculate bounding rectangle
+  const calculateBoundingRectangle = useCallback(() => {
+    let minX = Number.MAX_VALUE;
+    let minY = Number.MAX_VALUE;
+    let maxX = Number.MIN_VALUE;
+    let maxY = Number.MIN_VALUE;
+
+    triangle.vertices.forEach((vertex) => {
+      minX = Math.min(minX, vertex[0]);
+      minY = Math.min(minY, vertex[1]);
+      maxX = Math.max(maxX, vertex[0]);
+      maxY = Math.max(maxY, vertex[1]);
+    });
+
+    return {
+      width: maxX - minX,
+      height: maxY - minY,
+      area: (maxX - minX) * (maxY - minY),
+    };
+  }, [triangle]);
+
   // Calculate scaling factor for triangle to better fit on canvas
   const determineScailingFactor = useCallback(() => {
-    const boundingCircleArea = Math.PI * Math.pow(triangle.circumradius, 2);
-    const scalingFactor = Math.sqrt((width * height) / boundingCircleArea) / 5;
+    const boundingRectangle = calculateBoundingRectangle();
+
+    const scalingFactor = Math.sqrt((width * height) / boundingRectangle.area) * 0.4;
     return scalingFactor;
-  }, [triangle, width, height]);
+  }, [width, height, calculateBoundingRectangle]);
 
   // Re-calculate vertices for shape to be centered on canvas
   const recalculateVertices = useCallback(() => {
-    const canvasCenterX = width / 2 - triangle.circumradius / 2 - 60;
-    const canvasCenterY = height / 2 - triangle.circumradius / 2 - 40;
+    const canvasCenterX = width / 2 - calculateBoundingRectangle().width;
+    const canvasCenterY = height / 2 - calculateBoundingRectangle().height;
+    const scalingFactor = determineScailingFactor();
+
     const calculatedVertices = triangle.vertices.map((vertex) => [
-      vertex[0] * determineScailingFactor() + canvasCenterX,
-      vertex[1] * determineScailingFactor() + canvasCenterY,
+      vertex[0] * scalingFactor + canvasCenterX,
+      vertex[1] * scalingFactor + canvasCenterY,
     ]);
-    calculatedVertices[0][0] = canvasCenterX;
-    calculatedVertices[0][1] = canvasCenterY;
+
     return calculatedVertices;
-  }, [triangle, width, height, determineScailingFactor]);
+  }, [triangle, width, height, determineScailingFactor, calculateBoundingRectangle]);
 
   // Draw triangle on canvas
   useEffect(() => {
